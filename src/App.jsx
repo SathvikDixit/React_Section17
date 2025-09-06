014e9dce3031c42187c4a250f18e96a78f5dc9c7
SathvikDixit <sathvikdixit21@gmail.com>
2025-09-06 14:07:51 +0530

    qwerqwer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Users from "./components/Users";
 
 const App = () => {
+  const [first, setfirst] = useState(second)
   return (
-    <div>
-      
+    <div className="w-[80%] bg-zinc-200 mt-5 mx-auto p-5">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users/:id" element={<Users />} />
+      </Routes>
+      Home
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
