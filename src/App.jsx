10e2b6b87197cf5a5b64b39549bc3f0e1e84f21f
SathvikDixit <sathvikdixit21@gmail.com>
2025-09-06 17:50:58 +0530

    Done with Seaction17

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Users from "./components/Users";
 
-
 const App = () => {
   return (
     <div className="w-[80%] bg-zinc-200 mt-5 mx-auto p-5">
